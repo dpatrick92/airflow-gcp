64c4f304e66d6a22bb58f1a8e850339410011dc4
Doug <doug@S05322.2d.hq>
2019-01-11 13:46:12 -0500

    updating httpdag to include hooks

--- airflow/dags/httpdag.py
+++ airflow/dags/httpdag.py
@@ -4,8 +4,12 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.http_operator import SimpleHttpOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.hooks import HttpHook
+
+
 import requests
 import config
+import json
 
 args = {
     'start_date': airflow.utils.dates.days_ago(2),
@@ -23,7 +27,14 @@ def puller(**kwargs):
 
     # get value_1
     v1 = ti.xcom_pull(key=None, task_ids='python_parse')
+    v1 = json.loads(v1)
+    return v1[0]['office']
 
+def httpcall(**kwargs):
+    api_hook = HttpHook(http_conn_id='http_default', method='GET')
+    resp = api_hook.run('')
+    resp = json.loads(resp.text)
+    print resp
 
 
 to = DummyOperator(task_id='dummy_task', dag=dag)
@@ -51,5 +62,11 @@ pull = PythonOperator(
     python_callable=puller,
 )
 
-t2 >> pull
+test = PythonOperator(
+    task_id = 'httphooker',
+    dag=dag,
+    python_callable=httpcall
+)
+
+t1 >> t2 >> pull
 
